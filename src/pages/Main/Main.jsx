c33a1578682232f6167d37730e8d767973833d2c
saba <marghanias@gmail.com>
2024-03-23 10:52:23 +0400

    add 1 version of news list

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,6 +3,7 @@ import NewsBanner from "../../components/NewsBanner/NewsBanner";
 import {useEffect, useState} from "react";
 
 import {getNews} from "../../api/apiNews";
+import NewsList from "../../components/NewsList/NewsList";
 
 const Main = () => {
   const [news, setNews] = useState([]);
@@ -19,7 +20,10 @@ const Main = () => {
   }, []);
 
   return (
-    <main className={styles.main}>{news.length > 0 ? <NewsBanner item={news[0]} /> : null}</main>
+    <main className={styles.main}>
+      {news.length > 0 ? <NewsBanner item={news[0]} /> : null}
+      <NewsList news={news} />
+    </main>
   );
 };
 

00b0c09caa9a4898bbbd7ecf78cc69fedebda0eb
saba <marghanias@gmail.com>
2024-04-10 07:34:42 +0400

    change api endpoint, creatie pagination structure and styles

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,36 +1,43 @@
-import styles from "./styles.module.css";
-import NewsBanner from "../../components/NewsBanner/NewsBanner";
-import NewsList from "../../components/NewsList/NewsList";
-import Skeleton from "../../components/Skeleton/Skeleton";
+import styles from './styles.module.css';
+import NewsBanner from '../../components/NewsBanner/NewsBanner';
+import NewsList from '../../components/NewsList/NewsList';
+import Skeleton from '../../components/Skeleton/Skeleton';
 
-import {useEffect, useState} from "react";
-import {getNews} from "../../api/apiNews";
+import {useEffect, useState} from 'react';
+import {getNews} from '../../api/apiNews';
+import Pagination from '../../components/Pagination/Pagination';
 
 const Main = () => {
   const [news, setNews] = useState([]);
   const [isLoading, setIsloading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = 10;
+  const pageSize = 10;
+
+  const fetchNews = async currentPage => {
+    try {
+      setIsloading(true);
+      const response = await getNews(currentPage, pageSize);
+      setNews(response.news);
+      setIsloading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    setIsloading(true);
-    const fetchNews = async () => {
-      try {
-        const response = await getNews();
-        setNews(response.news);
-        setIsloading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchNews();
-  }, []);
+    fetchNews(currentPage);
+  }, [currentPage]);
 
   return (
     <main className={styles.main}>
       {news.length > 0 && !isLoading ? (
         <NewsBanner item={news[0]} />
       ) : (
-        <Skeleton type={"banner"} count={1} />
+        <Skeleton type={'banner'} count={1} />
       )}
-      {!isLoading ? <NewsList news={news} /> : <Skeleton type={"item"} count={10} />}
+      <Pagination totalPages={totalPages} />
+      {!isLoading ? <NewsList news={news} /> : <Skeleton type={'item'} count={10} />}
     </main>
   );
 };
